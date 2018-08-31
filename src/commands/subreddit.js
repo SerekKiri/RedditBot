8973176aba5dc1b1727a530cc5e731d82df8376d
Unknown <kacper.potyrala@gmail.com>
2018-08-31 15:08:21 +0200

    Working with api

--- src/commands/subreddit.js
+++ src/commands/subreddit.js
@@ -1,8 +1,15 @@
+const axios = require('axios');
+
+
 function find(message) {
     let mes = message.content.slice(12)
     if (mes === '') {
         message.reply('You need to type subreddit name here!')
     } else  {
+        const json = axios.get(`https://www.reddit.com/r/${mes}/new.json?limit=1`).then(function (response) {
+            // handle success
+            console.log(response)
+          })
         console.log(mes)
     }
 }
