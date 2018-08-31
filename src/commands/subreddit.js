0f0d37669823b26060e6a59eff173fee39fdac3a
Unknown <kacper.potyrala@gmail.com>
2018-08-31 17:11:45 +0200

    First design

--- src/commands/subreddit.js
+++ src/commands/subreddit.js
@@ -1,16 +1,37 @@
-const axios = require('axios');
+const axios = require('axios')
+const TurndownService = require('turndown')
+const turndownService = new TurndownService()
 
 
-function find(message) {
+async function find(message) {
     let mes = message.content.slice(12)
     if (mes === '') {
         message.reply('You need to type subreddit name here!')
     } else  {
-        const json = axios.get(`https://www.reddit.com/r/${mes}/new.json?limit=1`).then(function (response) {
+            const json = await axios.get(`https://www.reddit.com/r/${mes}/new.json?limit=2`).then(function (response) {
             // handle success
-            console.log(response)
+            text = response.data.data.children[0].data
+            const date = new Date(text.created_utc * 1000)
+            const embed = {
+                title: `${text.title}`,
+                url: `https://www.reddit.com${text.permalink}`,
+                author: {
+                    name: text.author,
+                    icon_url: "https://i.kym-cdn.com/photos/images/newsfeed/000/919/691/9e0.png"
+                  },
+                description: turndownService.turndown(text.selftext),
+                timestamp: date,
+                imgage: {
+                    url: ``
+                },
+                color: 16729344,
+                footer: {
+                  text: 'Reddit Bot by SerekKiri & MiXereK',
+                  icon_url: "https://cdn.discordapp.com/avatars/485047416291065859/ac0087022698709d0c7b26361e056bf9.png?size=256"
+                },
+              }
+              message.channel.send({ embed })
           })
-        console.log(mes)
     }
 }
 
