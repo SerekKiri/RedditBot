1a01f2c87211a0e6463ffbcdd914aedbc9370dfc
SerekKiri <kacper.potyrala@gmail.com>
2018-09-07 20:24:41 +0200

    Add random post

---
+++ src/commands/random.js
@@ -0,0 +1,46 @@
+const axios = require("axios");
+const redditPostToEmbed = require("../utils/redditPostToEmbed");
+ 
+async function find(message) {
+  let mes = message.content.slice(11);
+  let args = mes.split(" ");
+
+  if (mes === "") {
+    message.reply("You need to type subreddit name here!");
+  } else {
+   try {
+    if (args[1] <= 10 && args[1] > 0) {
+        let res
+        if(args[1] === null) {
+            res = await axios.get(
+                `https://www.reddit.com/r/random/top.json?limit=1`
+              );
+        } else {
+            res = await axios.get(
+                `https://www.reddit.com/r/random/top.json?limit=${args[1]}`
+              );
+        }
+      
+  
+      const posts = res.data.data.children;
+      if (posts.lenght == 0) {
+        message.reply(`Nothing new in **${args[0]}** :confused: `);
+        return;
+      } 
+  
+      for (const post of posts) {
+          const embed = redditPostToEmbed(post)
+          message.channel.send( { embed } )
+      }
+    } else {
+      message.reply(`I can't send you more then **10** messages :confused:`)
+    } 
+    
+   } catch(Error) {
+        console.log(Error)
+        message.reply(`No subreddits named **${mes}** :confused: `)
+        }
+    }
+  }
+
+module.exports = find;
