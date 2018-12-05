e9e77c3e159f8e797ce2aa3b12e42e8fd09a7389
Unknown <kacper.potyrala@gmail.com>
2018-12-05 21:10:23 +0100

    feat: Invite command

--- src/commands/invite.js
+++ src/commands/invite.js
@@ -1,22 +1,23 @@
+const TurndownService = require('turndown')
+const turndownService = new TurndownService()
 
-
-async function invite(client) {
+async function invite(message) {
     try {
-        client.generateInvite(['SEND_MESSAGES', 'MANAGE_GUILD']).then(link => {
-                const embed = {
-                    title: 'I generated a link for you:',
-                    description: turndownService.turndown(`<a href="${link}">Click here to invite me!</a>`),
-                    color: 1554076,
+            const embed = {
+                    title: 'I have a link for you:',
+                    description: turndownService.turndown(`<a href="https://discordapp.com/oauth2/authorize?client_id=485047416291065859&scope=bot&permissions=0">Click here to invite me!</a>`),
+                    color: 16729344,
                     footer: {
                     icon_url: 'https://cdn.discordapp.com/avatars/485047416291065859/ac0087022698709d0c7b26361e056bf9.png?size=256',
-                    text: 'Reddit Bot by SerekKiri & MiXerek'
-                    }
+                    text: 'Reddit Bot by SerekKiri & MiXerek',
+                }
             }
-            message.channel.send({ embed })
-            })
-        } catch(error) {
-            console.log(error)
-        }
+
+        await message.channel.send( { embed } )
+
+    } catch(error) {
+        console.log(error)
     }
+}
 
 module.exports = invite
