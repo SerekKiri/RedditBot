1a2229f3cf1afb178eb88713bae58fde50e5b368
SerekKiri <kacper.potyrala@gmail.com>
2019-04-28 14:16:10 +0200

    fix: Fixed typo in package and remove ussles code from index

--- src/index.js
+++ src/index.js
@@ -3,29 +3,11 @@ const config = require('./config/config.json')
 
 const client = new Discord.Client()
 
-// variables
-const package = require('../package.json')
-
 // commands imports
 const cmds = require('./commands/index')
 
 client.on('ready', () => {
-    const size = client.guilds.size
-    const users = client.users.size
-
-    const presence = [
-        "reddit.com",
-        "Mention me for help",
-        package.version + " version",
-        size + " servers",
-        users + " users"
-    ]
-
   console.log(`Reddit Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)
-    setInterval(() => {
-        const random = Math.floor(Math.random() * (presence.length - 1) + 1);
-        client.user.setActivity(presence[random]);
-    }, 7000)
 });
 
 
