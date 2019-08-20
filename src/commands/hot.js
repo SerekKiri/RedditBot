0c5991479531c9e5d0fc5a4ca6ce560489ef5627
SerekKiri <kacper.potyrala@gmail.com>
2019-08-20 20:02:49 +0200

    fix: Fixed mentioning by bot while responding

--- src/commands/hot.js
+++ src/commands/hot.js
@@ -1,49 +1,53 @@
-const axios = require("axios")
-const redditPostToEmbed = require("../utils/redditPostToEmbed")
- 
+const axios = require("axios");
+const redditPostToEmbed = require("../utils/redditPostToEmbed");
+
 async function find(message) {
-  let mes = message.content.slice(11)
-  let args = mes.split(" ")
+  let mes = message.content.slice(11);
+  let args = mes.split(" ");
 
   if (mes === "") {
     message.reply("You need to type subreddit name here!");
   } else {
-   try {
-    if (args[1] !== undefined && args[1] > 10) {
-      message.reply(`I can't send you more than **10** messages :confused:`)
-    } else {
-      let res
-      if(args[1] === undefined) {
-            res = await axios.get(
-                `https://www.reddit.com/r/${args[0]}/hot.json?limit=1&sort=new`
-              ) 
+    try {
+      if (args[1] !== undefined && args[1] > 10) {
+        message.reply(`I can't send you more than **10** messages :confused:`);
+      } else {
+        let res;
+        if (args[1] === undefined) {
+          res = await axios.get(
+            `https://www.reddit.com/r/${args[0]}/hot.json?limit=1&sort=new`
+          );
         } else {
-            res = await axios.get(
-                `https://www.reddit.com/r/${args[0]}/hot.json?limit=${args[1]}&sort=new`
-              )
+          res = await axios.get(
+            `https://www.reddit.com/r/${args[0]}/hot.json?limit=${
+              args[1]
+            }&sort=new`
+          );
         }
-      
-  
-      const posts = res.data.data.children;
-      if (posts.length == 0) {
-        return message.reply(`There are no hot posts on **${args[0]}** :confused: `)
-      } 
-  
-      for (const post of posts) {
+
+        const posts = res.data.data.children;
+        if (posts.length == 0) {
+          return message.reply(
+            `There are no hot posts on **${args[0]}** :confused: `
+          );
+        }
+
+        for (const post of posts) {
           if (post.data.over_18 === true && message.channel.nsfw === false) {
-            return message.reply(`This post is NSFW! Try get it on NSFW channel! :confused:`)
-            } else {
-            const embed = redditPostToEmbed(post)
-            await message.channel.send( { embed } )
+            return message.reply(
+              `This post is NSFW! Try get it on NSFW channel! :confused:`
+            );
+          } else {
+            const embed = redditPostToEmbed(post);
+            await message.channel.send({ embed });
           }
-      }
-    } 
-    
-   } catch(Error) {
-        console.log(Error)
-        message.reply(`No subreddits named **${mes}** :confused: `)
         }
+      }
+    } catch (Error) {
+      console.log(Error);
+      message.reply("No subreddits named `" + mes + "` :confused:");
     }
   }
+}
 
-module.exports = find
+module.exports = find;
--- src/commands/random.js
+++ src/commands/random.js
@@ -1,36 +1,37 @@
-const axios = require("axios")
-const redditPostToEmbed = require("../utils/redditPostToEmbed")
- 
-async function find(message) {
-  let mes = message.content.slice(14)
-  let args = mes.split(" ")
+const axios = require("axios");
+const redditPostToEmbed = require("../utils/redditPostToEmbed");
 
-   try {
+async function find(message) {
+  let mes = message.content.slice(14);
+  let args = mes.split(" ");
+
+  try {
     if (args[0] !== undefined && args[0] > 10) {
-      message.reply(`I can't send you more than **10** messages :confused:`)
+      message.reply(`I can't send you more than **10** messages :confused:`);
     } else {
-          let res = await axios.get(
-            `https://www.reddit.com/r/random/top.json?limit=1`
-          )
-          const posts = res.data.data.children
+      let res = await axios.get(
+        `https://www.reddit.com/r/random/top.json?limit=1`
+      );
+      const posts = res.data.data.children;
       if (posts.length == 0) {
-        return message.reply(`Nothing new in **${args[0]}** :confused: `)
-      } 
-  
+        return message.reply(`Nothing new in **${args[0]}** :confused: `);
+      }
+
       for (const post of posts) {
         if (post.data.over_18 === true && message.channel.nsfw === false) {
-          return message.reply(`This post is NSFW! Try get it on NSFW channel! :confused:`)
+          return message.reply(
+            `This post is NSFW! Try get it on NSFW channel! :confused:`
+          );
         } else {
-          const embed = redditPostToEmbed(post)
-          message.channel.send( { embed } )
+          const embed = redditPostToEmbed(post);
+          message.channel.send({ embed });
         }
       }
     }
-    
-   } catch(Error) {
-        console.log(Error)
-        message.reply(`No subreddits named **${mes}** :confused: `)
-        }
+  } catch (Error) {
+    console.log(Error);
+    message.reply("No subreddits named `" + mes + "` :confused:");
   }
+}
 
-module.exports = find
+module.exports = find;
